8b0b8043972594902726f399fb9b175318a17a97
peter <>
2016-09-30 18:08:12 +0300

    add request file api method

---
+++ scorocode_sdk/src/main/java/ru/profit_group/scorocode_sdk/Requests/files/RequestFile.java
@@ -0,0 +1,69 @@
+package ru.profit_group.scorocode_sdk.Requests.files;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Created by Peter Staranchuk on 5/10/2016
+ */
+
+public class RequestFile {
+    private String app;
+    private String cli;
+    private String acc;
+    private String sess;
+    private String coll;
+    private String docId;
+    private String field;
+    private String file;
+
+    public RequestFile(
+            @NonNull String app,
+            @NonNull String cli,
+            @NonNull String acc,
+            @NonNull String sess,
+            @NonNull String coll,
+            @NonNull String docId,
+            @NonNull String field,
+            @NonNull String file) {
+        this.app = app;
+        this.cli = cli;
+        this.acc = acc;
+        this.sess = sess;
+        this.coll = coll;
+        this.docId = docId;
+        this.field = field;
+        this.file = file;
+    }
+
+    public String getApp() {
+        return app;
+    }
+
+    public String getCli() {
+        return cli;
+    }
+
+    public String getAcc() {
+        return acc;
+    }
+
+    public String getSess() {
+        return sess;
+    }
+
+    public String getColl() {
+        return coll;
+    }
+
+    public String getDocId() {
+        return docId;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getFile() {
+        return file;
+    }
+}
