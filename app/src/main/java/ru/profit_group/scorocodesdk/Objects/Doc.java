ca4a32a6b380fac16b1b6ec98880d7c49e55691d
peter <>
2016-09-30 14:09:30 +0300

    add objects for requests

---
+++ app/src/main/java/ru/profit_group/scorocodesdk/Objects/Doc.java
@@ -0,0 +1,18 @@
+package ru.profit_group.scorocodesdk.Objects;
+
+import java.util.HashMap;
+
+/**
+ * Created by peter on 25/09/16.
+ */
+public class Doc {
+    private HashMap<String, String> doc;
+
+    public HashMap<String, String> getDoc() {
+        return doc;
+    }
+
+    public void setDoc(HashMap<String, String> doc) {
+        this.doc = doc;
+    }
+}
---
+++ app/src/main/java/ru/profit_group/scorocodesdk/Objects/Query.java
@@ -0,0 +1,7 @@
+package ru.profit_group.scorocodesdk.Objects;
+
+/**
+ * Created by peter on 25/09/16.
+ */
+public class Query {
+}
