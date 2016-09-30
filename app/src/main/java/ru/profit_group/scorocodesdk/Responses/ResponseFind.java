5b7385454ba8028ad3724f592b3f1609269efc7e
peter <>
2016-09-30 14:08:08 +0300

    add  find api method

---
+++ app/src/main/java/ru/profit_group/scorocodesdk/Responses/ResponseFind.java
@@ -0,0 +1,43 @@
+package ru.profit_group.scorocodesdk.Responses;
+
+/**
+ * Created by peter on 25/09/16.
+ */
+public class ResponseFind {
+    private boolean error;
+    private String errCode;
+    private String errMsg;
+    private String result;
+
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
+
+    public String getErrCode() {
+        return errCode;
+    }
+
+    public void setErrCode(String errCode) {
+        this.errCode = errCode;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    public void setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
