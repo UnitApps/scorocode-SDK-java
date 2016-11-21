4e6ab253ccf1421fcd2ca55101716218b9fcca49
PeterStaranchuk <petr.staranchuk@gmail.com>
2016-11-21 14:57:39 +0300

    rename Sort class to Sort info class make info classes implements Serializable interface.
    incapsulate  Map s inside objects

--- scorocode_sdk/src/main/java/ru/profit_group/scorocode_sdk/scorocode_objects/QueryInfo.java
+++ scorocode_sdk/src/main/java/ru/profit_group/scorocode_sdk/scorocode_objects/QueryInfo.java
@@ -1,10 +1,30 @@
 package ru.profit_group.scorocode_sdk.scorocode_objects;
 
+import android.support.annotation.NonNull;
+
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by Peter Staranchuk on 10/13/16
  */
-public class QueryInfo extends HashMap<String, Object> {
+public class QueryInfo implements Serializable {
+    private Map<String, Object> info;
+
+    public QueryInfo() {
+        this.info = new HashMap<>();
+    }
+
+    public QueryInfo(Map<String, Object> info) {
+        this.info = info;
+    }
+
+    @NonNull
+    public Map<String, Object> getInfo() {
+        if(info == null) {
+            return new HashMap<>();
+        }
+        return info;
+    }
 }
