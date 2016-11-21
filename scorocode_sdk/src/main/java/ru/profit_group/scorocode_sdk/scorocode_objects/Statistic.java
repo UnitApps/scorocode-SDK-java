d6930ea3bb27128928ef5796085f0c034fe22ca0
PeterStaranchuk <petr.staranchuk@gmail.com>
2016-11-21 15:00:41 +0300

    add serializable in Script, Statistic, Update and User classes

--- scorocode_sdk/src/main/java/ru/profit_group/scorocode_sdk/scorocode_objects/Statistic.java
+++ scorocode_sdk/src/main/java/ru/profit_group/scorocode_sdk/scorocode_objects/Statistic.java
@@ -1,5 +1,7 @@
 package ru.profit_group.scorocode_sdk.scorocode_objects;
 
+import java.io.Serializable;
+
 import ru.profit_group.scorocode_sdk.Callbacks.CallbackApplicationStatistic;
 import ru.profit_group.scorocode_sdk.Responses.statistic.ResponseAppStatistic;
 import ru.profit_group.scorocode_sdk.ScorocodeSdk;
@@ -8,7 +10,7 @@ import ru.profit_group.scorocode_sdk.ScorocodeSdk;
  * Created by Peter Staranchuk on 10/21/16
  */
 
-public class Statistic {
+public class Statistic implements Serializable {
 
     public void getApplicationStatistic(CallbackApplicationStatistic callbackApplicationStatistic) {
         ScorocodeSdk.getApplicationStatistic(callbackApplicationStatistic);
